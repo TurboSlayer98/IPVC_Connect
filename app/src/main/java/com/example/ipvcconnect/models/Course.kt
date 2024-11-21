9818a1a9ce42636da40a56df607296de39b67103
Pedro Simões <pedrosimoes@ipvc.pt>
2024-11-21 16:16:18 +0000

    app2

--- app/src/main/java/com/example/ipvcconnect/models/Course.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ipvcconnect.models
-
-data class Course(
-    val id: Int,
-    val name: String,
-    val description: String,
-    val schoolId: Int,
-    val imageUrl: String
-) 
--- app/src/main/java/com/example/ipvcconnect/models/School.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ipvcconnect.models
-
-data class School(
-    val id: Int,
-    val name: String,
-    val address: String,
-    val description: String,
-    val imageUrl: String
-) 
