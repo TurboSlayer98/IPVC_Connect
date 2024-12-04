c81b2eefb1cc7e8f8f5b5c421452e253046c59a8
TurboSlayer98 <hffm98@hotmail.com>
2024-12-04 16:10:36 +0000

    #20

--- app/src/main/java/com/example/ipvcconnect/InfoEmpresaActivity.kt
+++ app/src/main/java/com/example/ipvcconnect/InfoEmpresaActivity.kt
@@ -11,8 +11,6 @@ import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.maps.CameraUpdateFactory
